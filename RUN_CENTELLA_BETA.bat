fada87940375e304febf30e43aac0b06b8e78323
BRAD STEVEN CENTELLA GUEVARA <centellabradsteven@gmail.com>
2026-08-10 16:13:58 -0400

    Pass launcher arguments through Windows batch wrapper

--- RUN_CENTELLA_BETA.bat
+++ RUN_CENTELLA_BETA.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -File ".\scripts\RUN_BETA.ps1"
+powershell -NoProfile -ExecutionPolicy Bypass -File ".\scripts\RUN_BETA.ps1" %*
 if errorlevel 1 pause
