7818f0de681f2e6d3b789e2260e9677161cebad4
BRAD STEVEN CENTELLA GUEVARA <centellabradsteven@gmail.com>
2026-08-11 17:55:10 -0400

    Switch CENTELLA frontend to V3 spatial interface

--- centella/ui/src/main.tsx
+++ centella/ui/src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppV2 from './AppV2';
-import './styles.css';
-import './v2.css';
+import AppV3 from './AppV3';
+import './stylesV3.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppV2 />
+    <AppV3 />
   </React.StrictMode>,
 );
