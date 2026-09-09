f3080276bf1741faaa6c7e316ae11c3e2926fbfa
BRAD STEVEN CENTELLA GUEVARA <centellabradsteven@gmail.com>
2026-09-09 18:43:27 -0400

    feat(v27): export tactical intelligence API

--- centella_analytics/__init__.py
+++ centella_analytics/__init__.py
@@ -14,6 +14,7 @@ from .possession import infer_possession_changes, possession_summary
 from .quality import QualityThresholds, tracking_quality
 from .serialization import load_xg_model, save_xg_model
 from .tactical import aggregate_block_metrics, block_metrics, pass_network, post_loss_pressure
+from .tactical_intelligence import tactical_intelligence
 from .types import BallSample, PlayerSample, TrackingFrame
 from .v25 import VisionIntelligencePipeline
 from .workload import WorkloadProfile, compute_workload
@@ -28,7 +29,7 @@ __all__ = [
     "ShotFeatures", "LogisticXGModel", "fit_xg", "save_xg_model", "load_xg_model",
     "GoalkeeperAnalytics", "WorkloadProfile", "compute_workload", "QualityThresholds", "tracking_quality",
     "load_tracking_csv", "load_events_json", "frames_from_native_outputs",
-    "player_passport", "role_suitability", "player_intelligence",
+    "player_passport", "role_suitability", "player_intelligence", "tactical_intelligence",
 ]
 
-__version__ = "26.0.0"
+__version__ = "27.0.0"
