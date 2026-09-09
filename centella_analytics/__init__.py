6a0dce7e7cf8a5971015b64748cbfd050292eafa
BRAD STEVEN CENTELLA GUEVARA <centellabradsteven@gmail.com>
2026-09-09 18:30:14 -0400

    feat(analytics): export tracking-to-event inference API

--- centella_analytics/__init__.py
+++ centella_analytics/__init__.py
@@ -3,6 +3,7 @@
 from .adapter import frames_from_native_outputs, load_events_json, load_tracking_csv
 from .advanced import field_tilt, progression_rate, transition_metrics
 from .engine import AnalyticsEngine
+from .event_inference import EventInferenceConfig, infer_events, infer_passes, infer_possession, infer_shots
 from .events import DuelEvent, EventTimeline, PassEvent, SetPieceEvent, ShotEvent
 from .expected import LogisticXGModel, ShotFeatures, fit_xg
 from .goalkeeping import GoalkeeperAnalytics
@@ -21,6 +22,7 @@ __all__ = [
     "block_metrics", "aggregate_block_metrics", "pass_network", "post_loss_pressure",
     "PitchControlConfig", "pitch_control", "field_tilt", "progression_rate", "transition_metrics",
     "infer_possession_changes", "possession_summary",
+    "EventInferenceConfig", "infer_events", "infer_passes", "infer_possession", "infer_shots",
     "ShotFeatures", "LogisticXGModel", "fit_xg", "save_xg_model", "load_xg_model",
     "GoalkeeperAnalytics", "WorkloadProfile", "compute_workload", "QualityThresholds", "tracking_quality",
     "load_tracking_csv", "load_events_json", "frames_from_native_outputs",
